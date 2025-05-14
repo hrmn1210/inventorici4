7dc8bef1889078dae751f312cfa9c7939e8afd99
hrmn1210 <asenja924@gmail.com>
2025-05-14 13:54:51 +0700

    template awak

---
+++ app/Config/Routes.php
@@ -0,0 +1,12 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Admin::login');
+
+$routes->get('/admin/login-admin', 'Admin::login');
+$routes->get('/admin/dashboard-admin', 'Admin::dashboard');
+$routes->post('/admin/autentikasi-login', 'Admin::autentikasi');
